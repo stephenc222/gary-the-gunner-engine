c0c2e7a79ccd871bf9115e69c870b328a29546cb
Stephen Collins <stephenc211@gmail.com>
2018-04-01 21:59:07 -0500

    initializes glew, set up quit event handling

--- src/main.c
+++ src/main.c
@@ -3,10 +3,18 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+typedef struct {
+  int isKeyDown;
+  int isMouseDown;
+  double mouseX;
+  double mouseY;
+  int whichKey;
+} GameInput;
+
 typedef struct t_Game {
   int numSceneRects;
   // GameMap* gameMap;
-  // GameInput* gameInput;
+  GameInput* gameInput;
   // GameMenu* gameMenu;
   // Player* player;
   // Boss* firstBoss;
@@ -18,29 +26,61 @@ typedef struct t_Game {
   void (*renderFunc)( struct t_Game* game);
 } Game;
 
+// game is a global data structure
+Game* game;
+
 int initGame(void);
 void update(Game* game, float dt);
 void render(GLFWwindow* window, Game* game);
 void cleanUp(GLFWwindow* window, Game* game);
-
+void handleInput(GLFWwindow* window, int key, int scancode, int action, int mods);
+void destroyGameInput(GameInput* gameInput);
 Game* newGame(void);
 void destroyGame(Game* game);
+GameInput* newGameInput();
+
+void handleInput(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+GameInput* newGameInput() {
+  
+  GameInput* gameInput = malloc(sizeof *gameInput);
+  gameInput->isKeyDown = 0;
+  gameInput->mouseX = 0;
+  gameInput->mouseY = 0;
+  gameInput->whichKey = 0;
+  gameInput->isMouseDown = 0;
+
+  return gameInput;
+}
+
+void destroyGameInput(GameInput* gameInput) {
+  if (gameInput) {
+    free(gameInput);
+  }
+}
 
 
 void destroyGame(Game* game) {
+  if (game->gameInput) {
+    destroyGameInput(game->gameInput);
+  }
   game = 0;
 }
 
 Game* newGame(void) {
   Game* game = malloc(sizeof *game);
-
+  game->gameInput = newGameInput();
   game->updateFunc = 0;
   game->renderFunc = 0;
   return game;
 }
 
 void render(GLFWwindow* window, Game* game) {
-  // printf("rendering...");
    /* Render here */
   glClear(GL_COLOR_BUFFER_BIT);
 
@@ -53,11 +93,18 @@ void update(Game* game, float dt) {
 }
 
 void cleanUp(GLFWwindow* window, Game* game) {
+  destroyGame(game);
   glfwDestroyWindow(window);
   glfwTerminate();
 }
 
 int initGame(void) {
+  glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
   GLFWwindow* window;
 
   /* Initialize the library */
@@ -74,10 +121,27 @@ int initGame(void) {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
-  Game* game = newGame();
+  glfwSwapInterval(1);
+  glewExperimental=1; // Needed in core profile
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    return -1;
+  }
 
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+  // NOTE: continue on from here:
+  // http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+
+  game = newGame();
+
+  // set up basic player events, will eventually be scene-dependent
+  glfwSetKeyCallback(window, handleInput);
 
   /* Loop until the user closes the window */
+
+  // this is the main loop of the game application
   while (!glfwWindowShouldClose(window))
   { 
     /* Poll for and process events */
@@ -86,7 +150,6 @@ int initGame(void) {
     float dt = 0;
     double lastTime = 0, currentTime;
 
-    // handleInput(renderer,game, &loop);
     currentTime = glfwGetTime();
     if ((currentTime - lastTime) < 1000) {
       dt = (currentTime - lastTime);
@@ -97,7 +160,6 @@ int initGame(void) {
   }
 
   cleanUp(window, game);
-  // glfwTerminate();
   return 0;
 }
 
